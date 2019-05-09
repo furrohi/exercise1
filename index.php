c0da92aae016da694ccaf742a8e7eecad2ecc1e5
Farrukh <khaitov@sheridancollege.ca>
2019-05-09 14:50:53 -0400

    added code to file

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to my website</title>
+    </head>
+    <body>
+        <h1>Exercise 1</h1>
+        <p>Hello World</p>
+    </body>
+</html>
